61f68487c9c23b22ba080c75675d3d3a527dcab9
박정규 <srk99279@naver.com>
2022-11-28 21:55:54 +0900

    feat : API 호출 및 리그 순위 표기
    API-FOOTBALL을 이용한 프리미어리그 순위 및 통계 애플리케이션 제작, 현재 리그 스탠딩을 표기하는 화면 제작.

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// const options = {
+//   method: "GET",
+//   headers: {
+//     "X-RapidAPI-Key": "c33b72b886mshad567ef50e3e4ebp102b2fjsn0acac325e2e8",
+//     "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
+//   },
+// };
+
+// fetch("https://api-football-v1.p.rapidapi.com/v3/timezone", options)
+//   .then((response) => response.json())
+//   .then((response) => console.log(response))
+//   .catch((err) => console.error(err));
