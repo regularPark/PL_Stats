1d88a21df0e59b0f2a055bc8374e4507d2f529cd
박정규 <srk99279@naver.com>
2022-11-29 18:58:30 +0900

    feat : JS -> ReactJS
    API 사용을 포기하면서 axios, cheerio 등 웹 스크래핑하는 라이브러리를 사용하기 위해 프로젝트를 처음부터 갈아엎을 수 밖에 없었음. Vanilla JS를 포기하고 React로 프로젝트 재생성.

---
+++ pl-stats/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ShowStandings from "./components/standings";
+
+function App() {
+  return (
+    <div className="App">
+      <ShowStandings />
+    </div>
+  );
+}
+
+export default App;
