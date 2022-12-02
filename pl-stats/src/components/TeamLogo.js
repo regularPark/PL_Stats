dda6dfa8ce7081fb2ebee1aced40a10bae177258
박정규 <srk99279@naver.com>
2022-12-02 09:54:14 +0900

    style : CSS 수정
    모바일 환경에서 순위표의 transition이 너무 과도하게 일어나는 것 수정

--- pl-stats/src/components/TeamLogo.js
+++ pl-stats/src/components/TeamLogo.js
@@ -2,67 +2,44 @@ const TeamLogo = (val) => {
   switch (val) {
     case "아스날":
       return "https://resources.premierleague.com/premierleague/badges/25/t3.png";
-      break;
     case "맨체스터 시티":
       return "https://resources.premierleague.com/premierleague/badges/25/t43.png";
-      break;
     case "뉴캐슬 Utd":
       return "https://resources.premierleague.com/premierleague/badges/25/t4.png";
-      break;
     case "토트넘":
       return "https://resources.premierleague.com/premierleague/badges/25/t6.png";
-      break;
     case "맨체스터 유나이티드":
       return "https://resources.premierleague.com/premierleague/badges/25/t1.png";
-      break;
     case "리버풀":
       return "https://resources.premierleague.com/premierleague/badges/25/t14.png";
-      break;
     case "브라이튼":
       return "https://resources.premierleague.com/premierleague/badges/25/t36.png";
-      break;
     case "첼시":
       return "https://resources.premierleague.com/premierleague/badges/25/t8.png";
-      break;
     case "풀햄":
       return "https://resources.premierleague.com/premierleague/badges/25/t54.png";
-      break;
     case "브렌트퍼드":
       return "https://resources.premierleague.com/premierleague/badges/25/t94.png";
-      break;
     case "크리스탈팰리스":
       return "https://resources.premierleague.com/premierleague/badges/25/t31.png";
-      break;
     case "아스톤 빌라":
       return "https://resources.premierleague.com/premierleague/badges/25/t7.png";
-      break;
     case "레스터":
       return "https://resources.premierleague.com/premierleague/badges/25/t13.png";
-      break;
     case "본머스":
       return "https://resources.premierleague.com/premierleague/badges/25/t91.png";
-      break;
     case "리즈 유나이티드":
       return "https://resources.premierleague.com/premierleague/badges/25/t2.png";
-      break;
-
     case "웨스트햄":
       return "https://resources.premierleague.com/premierleague/badges/25/t21.png";
-      break;
     case "에버튼":
       return "https://resources.premierleague.com/premierleague/badges/25/t11.png";
-      break;
     case "노팅엄":
       return "https://resources.premierleague.com/premierleague/badges/25/t17.png";
-      break;
-
     case "사우샘프턴":
       return "https://resources.premierleague.com/premierleague/badges/25/t20.png";
-      break;
     case "울버햄튼":
       return "https://resources.premierleague.com/premierleague/badges/25/t39.png";
-      break;
-
     default:
       break;
   }
