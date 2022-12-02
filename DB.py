4288eaed28438518ec46cf1a7be1d86de715d527
박정규 <srk99279@naver.com>
2022-12-02 20:35:49 +0900

    refactor : 한글 -> 영어
    원활한 실행 위해 변경

--- DB.py
+++ DB.py
@@ -31,8 +31,8 @@ teams = league_table.find_all('td')
 # SUCCESS TABLE
 cnt = 0
 for i in range(0, 40, 2):
-    ref_std = db.reference('최신 리그 순위/'+ str(int(i / 2) + 1))
-    ref_std.update({'순위' : teams[i].text, '팀 이름' : teams[i+1].text, '경기수' : teams[40 + cnt*6].text,'승리' : teams[40 + cnt*6 + 1].text, '무승부' : teams[40 + cnt*6 + 2].text, '패배' : teams[40 + cnt*6 + 3].text, '승점' : teams[40 + cnt*6 + 4].text})
+    ref_std = db.reference('league_rank/'+ str(int(i / 2) + 1))
+    ref_std.update({'rank' : teams[i].text, 'team_name' : teams[i+1].text, 'G' : teams[40 + cnt*6].text,'W' : teams[40 + cnt*6 + 1].text, 'D' : teams[40 + cnt*6 + 2].text, 'L' : teams[40 + cnt*6 + 3].text, 'Pt' : teams[40 + cnt*6 + 4].text})
 
 PLAYER_RANK = "https://www.scoreman.vip/football/database/playertech-36"
 
@@ -58,28 +58,28 @@ players_tp_stats = players_tp_rank.find_all('td')
 print(players_assist_stats[2].text)
 
 for i in range(0, 10):
-    ref_player = db.reference('선수 통계/')
+    ref_player = db.reference('player_stats/')
     ref_player.update({
-        "득점 순위/" + str(i+1) + "/순위" : players_goal_stats[4*i].text,
-        "득점 순위/" + str(i+1) + "/이름" : players_goal_stats[4*i + 1].text.split('\n')[1],
-        "득점 순위/" + str(i+1) + "/팀 이름" : players_goal_stats[4*i + 1].text.split('\n')[2],
-        "득점 순위/" + str(i+1) + "/골" : players_goal_stats[4*i + 2].text,
-        "도움 순위/" + str(i+1) + "/순위" : players_assist_stats[3*i].text,
-        "도움 순위/" + str(i+1) + "/이름" : players_assist_stats[3*i + 1].text.split('\n')[1],
-        "도움 순위/" + str(i+1) + "/팀 이름" : players_assist_stats[3*i + 1].text.split('\n')[2],
-        "도움 순위/" + str(i+1) + "/도움" : players_assist_stats[3*i + 2].text,
-        "키 패스 순위/" + str(i+1) + "/순위" : players_kp_stats[3*i].text,
-        "키 패스 순위/" + str(i+1) + "/이름" : players_kp_stats[3*i + 1].text.split('\n')[1],
-        "키 패스 순위/" + str(i+1) + "/팀 이름" : players_kp_stats[3*i + 1].text.split('\n')[2],
-        "키 패스 순위/" + str(i+1) + "/키 패스" : players_kp_stats[3*i + 2].text,
-        "드리블 순위/" + str(i+1) + "/순위" : players_db_stats[4*i].text,
-        "드리블 순위/" + str(i+1) + "/이름" : players_db_stats[4*i + 1].text.split('\n')[1],
-        "드리블 순위/" + str(i+1) + "/팀 이름" : players_db_stats[4*i + 1].text.split('\n')[2],
-        "드리블 순위/" + str(i+1) + "/드리블" : players_db_stats[4*i + 2].text,
-        "패스 순위/" + str(i+1) + "/순위" : players_tp_stats[4*i].text,
-        "패스 순위/" + str(i+1) + "/이름" : players_tp_stats[4*i + 1].text.split('\n')[1],
-        "패스 순위/" + str(i+1) + "/팀 이름" : players_tp_stats[4*i + 1].text.split('\n')[2],
-        "패스 순위/" + str(i+1) + "/패스" : players_tp_stats[4*i + 2].text,
+        "score_rank/" + str(i+1) + "/rank" : players_goal_stats[4*i].text,
+        "score_rank/" + str(i+1) + "/name" : players_goal_stats[4*i + 1].text.split('\n')[1],
+        "score_rank/" + str(i+1) + "/team_name" : players_goal_stats[4*i + 1].text.split('\n')[2],
+        "score_rank/" + str(i+1) + "/goal" : players_goal_stats[4*i + 2].text,
+        "assist_rank/" + str(i+1) + "/rank" : players_assist_stats[3*i].text,
+        "assist_rank/" + str(i+1) + "/name" : players_assist_stats[3*i + 1].text.split('\n')[1],
+        "assist_rank/" + str(i+1) + "/team_name" : players_assist_stats[3*i + 1].text.split('\n')[2],
+        "assist_rank/" + str(i+1) + "/assist" : players_assist_stats[3*i + 2].text,
+        "key_pass_rank/" + str(i+1) + "/rank" : players_kp_stats[3*i].text,
+        "key_pass_rank/" + str(i+1) + "/name" : players_kp_stats[3*i + 1].text.split('\n')[1],
+        "key_pass_rank/" + str(i+1) + "/team_name" : players_kp_stats[3*i + 1].text.split('\n')[2],
+        "key_pass_rank/" + str(i+1) + "/key_pass" : players_kp_stats[3*i + 2].text,
+        "dribble_rank/" + str(i+1) + "/rank" : players_db_stats[4*i].text,
+        "dribble_rank/" + str(i+1) + "/name" : players_db_stats[4*i + 1].text.split('\n')[1],
+        "dribble_rank/" + str(i+1) + "/team_name" : players_db_stats[4*i + 1].text.split('\n')[2],
+        "dribble_rank/" + str(i+1) + "/dribble" : players_db_stats[4*i + 2].text,
+        "pass_rank/" + str(i+1) + "/rank" : players_tp_stats[4*i].text,
+        "pass_rank/" + str(i+1) + "/name" : players_tp_stats[4*i + 1].text.split('\n')[1],
+        "pass_rank/" + str(i+1) + "/team_name" : players_tp_stats[4*i + 1].text.split('\n')[2],
+        "pass_rank/" + str(i+1) + "/pass" : players_tp_stats[4*i + 2].text,
     })
 
 
@@ -111,21 +111,21 @@ teams_foul_stats = team_foul_rank.find_all('tr', {'id' : 'Total_FOULS'})
 print(teams_score_stats[0].text)
 
 for i in range(0,20):
-    ref_team = db.reference('팀 통계/')
+    ref_team = db.reference('team_stats/')
     ref_team.update({
-        "팀 득점 순위/" + str(i+1) + "/순위" : teams_score_stats[i].text.split('\n')[1],
-        "팀 득점 순위/" + str(i+1) + "/팀 이름" : teams_score_stats[i].text.split('\n')[2],
-        "팀 득점 순위/" + str(i+1) + "/득점" : teams_score_stats[i].text.split('\n')[3],
-        "팀 실점 순위/" + str(i+1) + "/순위" : teams_miss_stats[i].text.split('\n')[1],
-        "팀 실점 순위/" + str(i+1) + "/팀 이름" : teams_miss_stats[i].text.split('\n')[2],
-        "팀 실점 순위/" + str(i+1) + "/실점" : teams_miss_stats[i].text.split('\n')[3],
-        "점유율 순위/" + str(i+1) + "/순위" : teams_pos_stats[i].text.split('\n')[1],
-        "점유율 순위/" + str(i+1) + "/팀 이름" : teams_pos_stats[i].text.split('\n')[2],
-        "점유율 순위/" + str(i+1) + "/점유율" : teams_pos_stats[i].text.split('\n')[3],
-        "유효 슈팅 순위/" + str(i+1) + "/순위" : teams_shot_stats[i].text.split('\n')[1],
-        "유효 슈팅 순위/" + str(i+1) + "/팀 이름" : teams_shot_stats[i].text.split('\n')[2],
-        "유효 슈팅 순위/" + str(i+1) + "/유효 슈팅" : teams_shot_stats[i].text.split('\n')[3],
-        "파울 순위/" + str(i+1) + "/순위" : teams_foul_stats[i].text.split('\n')[1],
-        "파울 순위/" + str(i+1) + "/팀 이름" : teams_foul_stats[i].text.split('\n')[2],
-        "파울 순위/" + str(i+1) + "/파울" : teams_foul_stats[i].text.split('\n')[3],
+        "team_score_rank/" + str(i+1) + "/rank" : teams_score_stats[i].text.split('\n')[1],
+        "team_score_rank/" + str(i+1) + "/team_name" : teams_score_stats[i].text.split('\n')[2],
+        "team_score_rank/" + str(i+1) + "/score" : teams_score_stats[i].text.split('\n')[3],
+        "team_miss_rank/" + str(i+1) + "/rank" : teams_miss_stats[i].text.split('\n')[1],
+        "team_miss_rank/" + str(i+1) + "/team_name" : teams_miss_stats[i].text.split('\n')[2],
+        "team_miss_rank/" + str(i+1) + "/miss" : teams_miss_stats[i].text.split('\n')[3],
+        "possession_rank/" + str(i+1) + "/rank" : teams_pos_stats[i].text.split('\n')[1],
+        "possession_rank/" + str(i+1) + "/team_name" : teams_pos_stats[i].text.split('\n')[2],
+        "possession_rank/" + str(i+1) + "/possession" : teams_pos_stats[i].text.split('\n')[3],
+        "shot_on_target_rank/" + str(i+1) + "/rank" : teams_shot_stats[i].text.split('\n')[1],
+        "shot_on_target_rank/" + str(i+1) + "/team_name" : teams_shot_stats[i].text.split('\n')[2],
+        "shot_on_target_rank/" + str(i+1) + "/shot_on_target" : teams_shot_stats[i].text.split('\n')[3],
+        "foul_rank/" + str(i+1) + "/rank" : teams_foul_stats[i].text.split('\n')[1],
+        "foul_rank/" + str(i+1) + "/team_name" : teams_foul_stats[i].text.split('\n')[2],
+        "foul_rank/" + str(i+1) + "/foul" : teams_foul_stats[i].text.split('\n')[3],
     })
