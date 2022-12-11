d2c5f1a0d9dc480e3c1cb9fe7599376824980ecf
박정규 <srk99279@naver.com>
2022-12-11 16:27:42 +0900

    feat : 문서 업데이트

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ require("firebase/compat/database");
 require("dotenv").config();
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
